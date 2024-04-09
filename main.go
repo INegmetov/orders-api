f56647acf224944e9d4246e332643d029224f046
INegmetov <negmetov.i@gmail.com>
2024-04-09 16:18:06 +0400

    refactoring code adn routes

--- main.go
+++ main.go
@@ -1,26 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"github.com/go-chi/chi/v5"
-	"net/http"
+	"github.com/inegmetov/orders-api/application"
 )
 
 func main() {
-	router := chi.NewRouter()
-
-	router.Get("/hello", basicHandler)
-
-	server := &http.Server{
-		Addr:    ":3000",
-		Handler: router,
-	}
-	err := server.ListenAndServe()
+	app := application.New()
+	err := app.Start(context.TODO())
 	if err != nil {
-		fmt.Println("Failed to listen to server", err)
+		fmt.Println("failed to start App:", err)
 	}
-}
-
-func basicHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello world!"))
 }
