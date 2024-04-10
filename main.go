bc171badda49a6b8f377e97681a0bb728ca89a4b
INegmetov <negmetov.i@gmail.com>
2024-04-10 15:09:38 +0400

    Data model for order added

--- main.go
+++ main.go
@@ -4,11 +4,15 @@ import (
 	"context"
 	"fmt"
 	"github.com/inegmetov/orders-api/application"
+	"os"
+	"os/signal"
 )
 
 func main() {
 	app := application.New()
-	err := app.Start(context.TODO())
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+	err := app.Start(ctx)
 	if err != nil {
 		fmt.Println("failed to start App:", err)
 	}
