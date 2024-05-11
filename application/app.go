e968664b5267e53f5c09e523a872c846e91236df
INegmetov <negmetov.i@gmail.com>
2024-05-11 19:43:25 +0400

    config env  comleted

--- application/app.go
+++ application/app.go
@@ -3,29 +3,37 @@ package application
 import (
 	"context"
 	"fmt"
-	"github.com/redis/go-redis/v9"
 	"net/http"
 	"time"
+
+	"github.com/redis/go-redis/v9"
 )
 
 type App struct {
 	router http.Handler
 	rdb    *redis.Client
+	config Config
 }
 
-func New() *App {
+func New(config Config) *App {
 	app := &App{
-		rdb: redis.NewClient(&redis.Options{}),
+		rdb: redis.NewClient(&redis.Options{
+			Addr: config.RedisAddress,
+		}),
+		config: config,
 	}
+
 	app.loadRoutes()
+
 	return app
 }
 
 func (a *App) Start(ctx context.Context) error {
 	server := &http.Server{
-		Addr:    ":3000",
+		Addr:    fmt.Sprintf(":%d", a.config.ServerPort),
 		Handler: a.router,
 	}
+
 	err := a.rdb.Ping(ctx).Err()
 	if err != nil {
 		return fmt.Errorf("failed to connect to redis: %w", err)
@@ -33,29 +41,31 @@ func (a *App) Start(ctx context.Context) error {
 
 	defer func() {
 		if err := a.rdb.Close(); err != nil {
-			fmt.Println("Failed to close redis", err)
+			fmt.Println("failed to close redis", err)
 		}
 	}()
 
-	fmt.Println("Starting server on port :3000")
+	fmt.Println("Starting server")
 
 	ch := make(chan error, 1)
 
 	go func() {
 		err = server.ListenAndServe()
 		if err != nil {
-			ch <- fmt.Errorf("failed to start server : %w", err)
+			ch <- fmt.Errorf("failed to start server: %w", err)
 		}
 		close(ch)
 	}()
 
 	select {
-	case err := <-ch:
+	case err = <-ch:
 		return err
 	case <-ctx.Done():
 		timeout, cancel := context.WithTimeout(context.Background(), time.Second*10)
 		defer cancel()
+
 		return server.Shutdown(timeout)
 	}
+
 	return nil
 }
